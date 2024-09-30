9be7a9c3b4241c59b03b9b6017444a424bc708b1
TobiasRW <tobiasrw98@gmail.com>
2024-09-30 20:47:20 +0200

    .

---
+++ src/app/components/ReccomendedBook.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function ReccomendedBook({ coverID, title }) {
+  const defaultCover = "/assets/img/default-cover.webp"; // Default cover image path
+
+  return (
+    <>
+      <img
+        src={coverID || defaultCover} // Google Books cover URL or default cover
+        alt={title}
+        className="w-1/5 rounded-md aspect-[2/3] object-cover"
+        onError={(e) => {
+          e.target.onerror = null;
+          e.target.src = defaultCover; // Fallback to default cover
+        }}
+      />
+    </>
+  );
+}
